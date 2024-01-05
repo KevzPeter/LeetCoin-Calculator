6743fb2b846198411ccefbb24f6840b37eb77341
KevzPeter <ultimatekevin1@gmail.com>
2024-01-06 01:38:40 +0530

    🔧 Updated rewards

--- utils/rewards.util.js
+++ utils/rewards.util.js
@@ -1,24 +1,32 @@
 const rewards = [
     {
-        "name" : "Leetcode Premium", 
-        "coins" : 5000
+        "name": "⏳ Time Travel Ticket",
+        "coins": 70
     },
     {
-        "name" : "Leetcode T-Shirt", 
-        "coins" : 6000
+        "name": "💲 30-Day Premium Subscription",
+        "coins": 5000
     },
     {
-        "name" : "Leetcode Kit", 
-        "coins" : 7800
+        "name": "🧢 Leetcode Cap",
+        "coins": 5400
     },
     {
-        "name" : "Leetcode Laptop Sleeve", 
-        "coins" : 8000
+        "name": "👕 Leetcode T-Shirt",
+        "coins": 6000
     },
     {
-        "name" : "Leetcode Big-O Notebook", 
-        "coins" : 9900
+        "name": "👕✨ Leetcode Kit",
+        "coins": 7800
+    },
+    {
+        "name": "💻 Leetcode Laptop Sleeve",
+        "coins": 8000
+    },
+    {
+        "name": "📓 Leetcode Big-O Notebook",
+        "coins": 9900
     },
 ]
 
-module.exports = {rewards};
+module.exports = { rewards };
